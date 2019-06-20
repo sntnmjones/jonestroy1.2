bb2a70a183affd43450f5e20a5e012f1c54d76b9
Troy Jones <sntnmjones@gmail.com>
2019-06-19 18:14:51 -0700

    content div scrollable.

--- production-site/jonestroy/src/components/Home.js
+++ production-site/jonestroy/src/components/Home.js
@@ -1,14 +1,67 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Menu from './Menu';
+import Hello from './Hello1'
+import '../css/Home.css'
 
 class Home extends Component {
     render() {
         return (
-            <div>
+            <div className='mainDiv'>
                 <Header />
-                <div>
+                <div className='secondDiv'>
                     <Menu />
+                    <div className='content'>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                    </div>
                 </div>
             </div>
         );
