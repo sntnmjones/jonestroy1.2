61a679a5c201fc0caa4ab122cc55b69ef8a4918c
Troy Jones <sntnmjones@gmail.com>
2019-06-19 17:28:08 -0700

    Changed Header css for div placement in Home.js

--- production-site/jonestroy/src/components/Header.js
+++ production-site/jonestroy/src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
     render() {
         return (
             <div className='header'>
-                <a href='/' className='link'>
+                <a href='/'  className='link'>
                     <h2>jonestroy.com</h2>
                 </a>
             </div>
