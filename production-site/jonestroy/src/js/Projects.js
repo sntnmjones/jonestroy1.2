528398f9cbe117e3ac041b04344d0f35656bf0ad
Troy Jones <sntnmjones@gmail.com>
2019-08-22 20:04:19 -0700

    Added python scraping project to 'Projects'

--- production-site/jonestroy/src/js/Projects.js
+++ production-site/jonestroy/src/js/Projects.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import { Row, Col, Card, Button } from 'react-bootstrap';
 
 function Projects() {
-  return (
-    <Container style={{backgroundColor: 'white'}}>
-        Coming soon!
-    </Container>
-  );
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Python Web Scraper</Card.Title>
+                            <Card.Text>
+                                Rudimentary web scraper using Beautiful Soup. Variables must be entered in code in main() before execution. Updates coming. To execute, use command `python3 lab9.py` in linux terminal.
+
+                            </Card.Text>
+                            <Button href='https://github.com/sntnmjones/CSCI344-Web-Scraper/blob/master/lab9.py'>Go to Github page</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default Projects;
